16b5dad85fbe50fb131584acad6639bc0c0d9917
Krish Dholakia <krrishdholakia@gmail.com>
2024-04-13 22:39:09 -0700

    updates

--- run_test.sh
+++ run_test.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# Function to run Locust test
-function run_all_cache_hits_locust_test() {
-    locust -f all_cache_hits.py --headless -u 20 -r 20 -H http://a472dc7c273fd47fd9a20434f463afd1-393291597.us-west-2.elb.amazonaws.com:4000/ -t 300 --csv load_test
-}
-
-
 function run_no_cache_hits_locust_test() {
-    locust -f no_cache_hits.py --headless -u 20 -r 20 -H http://a472dc7c273fd47fd9a20434f463afd1-393291597.us-west-2.elb.amazonaws.com:4000/ -t 300 --csv load_test
-}
-
-function run_cache_off_locust_test() {
-    locust -f no_cache.py --headless -u 20 -r 20 -H http://a472dc7c273fd47fd9a20434f463afd1-393291597.us-west-2.elb.amazonaws.com:4000/ -t 300 --csv load_test
-}
-
-function run_simple_openai_proxy_locust_test() {
-    locust -f no_cache_hits.py --headless -u 20 -r 20 -H https://simplelitellmproxy-production.up.railway.app/openai/ -t 300 --csv load_test
+    locust -f no_cache_hits.py --headless -u 20 -r 20 -H http://34.106.146.156/ -t 300 --csv load_test
 }
 
 # Deploy your project (assuming deployment commands are here)
@@ -29,12 +15,6 @@ ls -lAh
 while true; do
     echo "Running tests..."
 
-    # All Cache hits test
-    run_all_cache_hits_locust_test
-
-    # Run the load test script
-    python3 interpret_load_test.py all_cache_hits
-
     # # Wait for 20 seconds
     # echo "Waiting for 20 seconds..."
     # sleep 20
@@ -45,13 +25,4 @@ while true; do
     # Run the load test script again
     python3 interpret_load_test.py no_cache_hits
 
-    # Cache off test
-    run_cache_off_locust_test
-
-    python3 interpret_load_test.py cache_off_test
-
-    # Simple OpenAI Proxy Load Test
-    run_simple_openai_proxy_locust_test
-
-    python3 interpret_load_test.py simple_openai_proxy
 done
